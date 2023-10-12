f2d611ce34bc3169a79c21ffdb6cd6e88507f30c
Daniel Gafni <danielgafni16@gmail.com>
2023-10-12 12:46:25 +0200

    :bug: move optional modules imports into try-except blocks

--- dagster_serde/__init__.py
+++ dagster_serde/__init__.py
@@ -1,9 +1,22 @@
 from dagster_serde._version import __version__
 from dagster_serde.io_managers.base import BaseSerdeUPathIOManager
-from dagster_serde.io_managers.json import JsonIOManager
 
 __all__ = [
     "BaseSerdeUPathIOManager",
-    "JsonIOManager",
     "__version__",
 ]
+
+
+try:
+    from dagster_serde.io_managers.json import JsonIOManager
+
+    __all__.append("JsonIOManager")
+except ImportError:
+    pass
+
+try:
+    from dagster_serde.io_managers.yaml import YamlIOManager
+
+    __all__.append("YamlIOManager")
+except ImportError:
+    pass
